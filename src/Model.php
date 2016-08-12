b26750b5e95ad5a4276b491a92151860844cc9e7
Febalist <febalist@gmail.com>
2016-08-12 16:32:02 +0300

    Static methods

--- src/Model.php
+++ src/Model.php
@@ -1,6 +1,7 @@
 <?php namespace Febalist\LaravelModel;
 
 use DB;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 use Jenssegers\Date\Date;
 
@@ -8,6 +9,8 @@ use Jenssegers\Date\Date;
  * @property integer id
  * @property Date    created_at
  * @property Date    updated_at
+ * @method static Builder where($column, $operator = null, $value = null, $boolean = 'and')
+ * @method static $this first($columns = ['*'])
  */
 class Model extends Eloquent
 {
