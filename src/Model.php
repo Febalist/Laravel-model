bd5187ece992795b6881188dfea1c8295f75dbde
Febalist <febalist@gmail.com>
2016-08-17 03:24:09 +0300

    Disabled cast before mutate

--- src/Model.php
+++ src/Model.php
@@ -48,13 +48,4 @@ class Model extends Eloquent
         return Date::parse($value);
     }
 
-    protected function mutateAttribute($key, $value)
-    {
-        if ($this->hasCast($key)) {
-            $value = $this->castAttribute($key, $value);
-        }
-        $value = parent::mutateAttribute($key, $value);
-        return $value;
-    }
-
 }
