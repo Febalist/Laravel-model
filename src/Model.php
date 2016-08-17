f0e0b65b37fbdfdc9e001fd71a85e8710b5fb07a
Febalist <febalist@gmail.com>
2016-08-17 14:30:36 +0300

    getJson & setJson methods

--- src/Model.php
+++ src/Model.php
@@ -48,4 +48,23 @@ class Model extends Eloquent
         return Date::parse($value);
     }
 
+    protected function getJson($key, $default = null)
+    {
+        $value = $this->getAttributeFromArray($key);
+        if (is_null($value)) {
+            return $default;
+        }
+        return $this->fromJson($value);
+    }
+
+    protected function setJson($key, $value)
+    {
+        if ($value instanceof Collection) {
+            $value = $value->toJson();
+        } else {
+            $value = $this->asJson($value);
+        }
+        $this->attributes[$key] = $value;
+    }
+
 }
