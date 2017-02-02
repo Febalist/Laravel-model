d0b635fc2b7ca43317e24cb34cdcb6ab39b02d17
Febalist <febalist@gmail.com>
2017-02-02 22:50:33 +0300

    Use Eloquent alias

--- src/Model.php
+++ src/Model.php
@@ -1,7 +1,8 @@
-<?php namespace Febalist\LaravelModel;
+<?php
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model as Eloquent;
+namespace Febalist\LaravelModel;
+
+use Eloquent;
 use Illuminate\Support\Collection;
 use Jenssegers\Date\Date;
 
@@ -9,19 +10,6 @@ use Jenssegers\Date\Date;
  * @property integer id
  * @property Date    created_at
  * @property Date    updated_at
- * @method static Builder where($column, $operator = null, $value = null, $boolean = 'and')
- * @method static Builder whereIn($column, $values, $boolean = 'and', $not = false)
- * @method static $this find($id, $columns = ['*'])
- * @method static $this findMany($ids, $columns = ['*'])
- * @method static $this findOrFail($id, $columns = ['*'])
- * @method static $this findOrNew($id, $columns = ['*'])
- * @method static $this first($columns = ['*'])
- * @method static $this firstOrFail($columns = ['*'])
- * @method static $this firstOrNew(array $attributes)
- * @method static $this firstOrCreate(array $attributes, array $values = [])
- * @method static $this updateOrCreate(array $attributes, array $values = [])
- * @method static Collection pluck($column, $key = null)
- * @method static $this chunk($count, callable $callback)
  */
 class Model extends Eloquent
 {
